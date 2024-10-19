c3c18a4ad464ec3304a5106e64e53d8d10cb8e25
ASUS <ASUS@DESKTOP-NB7MT4D>
2024-10-20 00:20:56 +0400

    TaskUDP

---
+++ TaskUdpClient/Client.cs
@@ -0,0 +1,41 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+//Istenilen file sechilir. servere gonderir.
+//server client uchun folder (IP Unvanina uygun folder), onun ichirisine download edir.
+//using UDP
+
+Console.WriteLine("Client...");
+
+var endPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 58758);
+var rep = new IPEndPoint(IPAddress.Any, 0);
+var client = new UdpClient();
+
+var path = @"C:\Users\ASUS\Desktop\Black.png"; // change Path this Path have not your pc
+
+if (Path.Exists(path))
+{
+    FileInfo file = new FileInfo(path);
+
+    var fileNameBytes = Encoding.UTF8.GetBytes(file.Name);
+    client.Send(fileNameBytes, fileNameBytes.Length, endPoint);
+    
+    var fileLengthBytes = Encoding.UTF8.GetBytes(file.Length.ToString());
+    client.Send(fileLengthBytes, fileLengthBytes.Length, endPoint);
+
+    using (var fs = new FileStream(path, FileMode.Open, FileAccess.Read))
+    {
+        var bytes = new byte[1024];
+        var len = 0;
+        while ((len = fs.Read(bytes, 0, bytes.Length)) > 0)
+        {
+            client.Send(bytes, len, endPoint);
+            client.Receive(ref rep);
+        }
+    }
+    Console.WriteLine("File sent");
+    Console.ReadKey();
+}
+else
+    Console.WriteLine("Have Not File With Your Path");
---
+++ TaskUdpServer/Server.cs
@@ -0,0 +1,52 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+var endPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 58758);
+var server = new UdpClient(endPoint);
+
+Console.WriteLine("Server is running");
+
+Task myTask = Task.Run(() =>
+{
+    var remoteEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+    while (true)
+    {
+        var fileNameBytes = server.Receive(ref remoteEndPoint);
+        var fileLengthBytes = server.Receive(ref remoteEndPoint);
+
+        var fileName = Encoding.UTF8.GetString(fileNameBytes);
+        int fileLen = int.Parse(Encoding.UTF8.GetString(fileLengthBytes));
+
+        var destination = Environment.CurrentDirectory;//vs2022 myproj directory
+        destination = Path.Combine(destination, $"{remoteEndPoint.Address}");
+
+        if (!Directory.Exists(destination))
+        {
+            Directory.CreateDirectory(destination);
+        }
+
+        var path = $"{destination}\\{DateTime.Now:HH.mm.ss}{Path.GetExtension(fileName)}";
+
+        using (var fStream = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Write))
+        {
+            var totalRecievedByte = 0;
+            while (true)
+            {
+                var fileByte = server.Receive(ref remoteEndPoint);
+                fStream.Write(fileByte);
+
+                totalRecievedByte += fileByte.Length;
+                server.Send(null, remoteEndPoint);
+
+                if (fileLen == totalRecievedByte)
+                    break;
+            }
+        }
+        Console.WriteLine("File Downloaded");
+    }
+});
+myTask.Wait();
+
+
